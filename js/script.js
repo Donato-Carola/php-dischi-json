b949f9ee94f27011a09eba647832ad8a2e30f577
Donato-Carola <don94@live.it>
2024-01-16 16:12:56 +0100

    primo lavoro su api json ...

--- js/script.js
+++ js/script.js
@@ -4,7 +4,28 @@
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        url:'./script.php',
+        albums:[],
       }
-    }
+    },
+       
+    methods: {
+      albumsGet(){
+        axios.get(this.url)
+        .then(response => {
+          this.albums = response.data;
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.error('Errore durante la richiesta API', error);
+        });
+      }
+    },
+
+    mounted() {
+      this.albumsGet();
+    },
+
+
+
   }).mount('#app')
---
+++ script.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+$albums = [
+    [
+        "image" => "./img/ACDC.jpg",
+        "title" => "Highway to Hell",
+        "artist" => "ACDC",
+        "year" => 1979
+    ],
+    [
+        "image" => "./img/ACDC.jpg",
+        "title" => "Highway to Hell",
+        "artist" => "ACDC",
+        "year" => 1979
+    ],
+    [
+        "image" => "./img/ACDC.jpg",
+        "title" => "Highway to Hell",
+        "artist" => "ACDC",
+        "year" => 1979
+    ],
+    [
+        "image" => "./img/ACDC.jpg",
+        "title" => "Highway to Hell",
+        "artist" => "ACDC",
+        "year" => 1979
+    ],
+    [
+        "image" => "./img/ACDC.jpg",
+        "title" => "Highway to Hell",
+        "artist" => "ACDC",
+        "year" => 1979
+    ],
+    [
+        "image" => "./img/ACDC.jpg",
+        "title" => "Highway to Hell",
+        "artist" => "ACDC",
+        "year" => 1979
+    ]
+];
+
+
+header('Content-Type: application/json');
+echo json_encode($albums);
