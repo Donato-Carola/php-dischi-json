4f847ab8172a5d5e6124ff7cfabd9a5a5d946822
Donato-Carola <don94@live.it>
2024-01-16 14:32:50 +0100

    creazione layout

--- index.php
+++ index.php
@@ -1,26 +1,41 @@
-
-
-
-
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>spoty</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.5/axios.min.js"> 
-
-
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>spoty</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.5/axios.min.js">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   <link rel="stylesheet" href="./css/style.css">
 
 
 </head>
+
 <body>
-    <div id="app">
+   <div id="app">
+      <header class="bg-black">
 
-    </div>
+         <img src="./img/spotylogo.png" alt="">
+
+      </header>
+
+      <main>
+         <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+         </div>
+      </main>
 
 
- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+   </div>
+
+
+   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </body>
+
 </html>
